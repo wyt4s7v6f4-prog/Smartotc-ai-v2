03bdc940165cffa1be3f17a983f741d935221850
wyt4s7v6f4-prog <wyt4s7v6f4@privaterelay.appleid.com>
2026-08-06 17:45:41 +0000

    Fix twelvedata syntax

--- backend/app/providers/twelvedata.py
+++ backend/app/providers/twelvedata.py
@@ -3,7 +3,7 @@ import pandas as pd
 import ta
 
 import os
-API_KEY = os.getenv("TWELVEDATA_API_KEY","afdc8f80c3374bb7a5130679e76e57ae"
+API_KEY = os.getenv("TWELVEDATA_API_KEY","afdc8f80c3374bb7a5130679e76e57ae")
 
 
 def get_forex_data(symbol="EUR/USD", interval="1min"):
